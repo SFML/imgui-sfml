7e3058a944397cc80c8147ebd37dccd3a41749d4
Christian Glöckner <cgloeckner@freenet.de>
2016-06-10 10:22:01 +0200

    color to vec4 conversion
    useful for e.g. `ImGui::TextColored(ImVec4(sf::Color::Green), "All changes are saved");`

--- imconfig-SFML.h
+++ imconfig-SFML.h
@@ -12,3 +12,14 @@
 	operator sf::Vector2<T>() const {									\
 		return sf::Vector2<T>{sf::Vector2f{x, y}};						\
 	}
+
+#define IM_VEC4_CLASS_EXTRA												\
+	ImVec4(sf::Color const const & c)									\
+		: ImVec4{static_cast<float>(c.r), static_cast<float>(c.g),		\
+			static_cast<float>(c.b), static_cast<float>(c.a)} {			\
+	}																	\
+	operator sf::Color() const {										\
+		return sf::Color{static_cast<sf::Uint8>(x),						\
+			static_cast<sf::Uint8>(y), static_cast<sf::Uint8>(z),		\
+			static_cast<sf::Uint8>(w)};									\
+	}
